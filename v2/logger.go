0eae16de04d485800b1d557abfb1170dc5b90e84
zheng peng <pengzheng@huishoubao.com.cn>
2023-04-12 16:12:27 +0800

    optimize

--- v2/logger.go
+++ v2/logger.go
@@ -1,10 +1,8 @@
 package logchan
 
 import (
-	"context"
 	"fmt"
 	"sync"
-	"time"
 
 	"github.com/pkg/errors"
 )
@@ -49,18 +47,21 @@ func (l *EmptyLogInfo) GetLevel() (name string) {
 // LogInfoChainBuffer 日志缓冲区,减少并发日志丢失情况
 var LogInfoChainBuffer int = 50
 
-// logInfoChain 日志传送通道，缓冲区满后,会丢弃日志
-var logInfoChain = make(chan LogInforInterface, LogInfoChainBuffer)
 var setLoggerWriteOnce sync.Once
 var sendLogInfoFn func(logInfo LogInforInterface) //只能初始化一次
-var doneChan chan struct{}
+var closeLogChan func()
 
 // SetLoggerWriter 设置日志处理函数，同时返回发送日志函数
-func SetLoggerWriter(ctx context.Context, handlerLogInfoFn func(logInfo LogInforInterface, typeName string, err error)) {
+func SetLoggerWriter(handlerLogInfoFn func(logInfo LogInforInterface, typeName string, err error)) {
 	if handlerLogInfoFn == nil {
 		return
 	}
+
 	setLoggerWriteOnce.Do(func() {
+		var doneChan = make(chan struct{}, 1) //设置缓冲后，不会阻塞当前携程
+		// logInfoChain 日志传送通道，缓冲区满后,会丢弃日志
+		var logInfoChain = make(chan LogInforInterface, LogInfoChainBuffer)
+		// 启动监听
 		go func() {
 			defer func() {
 				doneChan <- struct{}{} // 通知日志写入结束
@@ -72,21 +73,11 @@ func SetLoggerWriter(ctx context.Context, handlerLogInfoFn func(logInfo LogInfor
 				}
 			}()
 			for logInfo := range logInfoChain {
-				func(logInfo LogInforInterface) {
-					handlerLogInfoFn(logInfo, logInfo.GetName(), logInfo.Error())
-				}(logInfo)
-
+				handlerLogInfoFn(logInfo, logInfo.GetName(), logInfo.Error())
 			}
 		}()
 
-		go func() {
-			select {
-			case <-ctx.Done():
-				close(logInfoChain)
-				return
-			}
-		}()
-
+		//封装发送句柄
 		sendLogInfoFn = func(logInfo LogInforInterface) {
 			select {
 			case logInfoChain <- logInfo:
@@ -96,7 +87,15 @@ func SetLoggerWriter(ctx context.Context, handlerLogInfoFn func(logInfo LogInfor
 
 			}
 		}
+
+		//封装关闭句柄
+		closeLogChan = func() {
+			close(logInfoChain)
+			<-doneChan
+		}
+
 	})
+
 	return
 }
 
@@ -107,20 +106,9 @@ func SendLogInfo(logInfo LogInforInterface) {
 	sendLogInfoFn(logInfo)
 }
 
-// UntilFinished 阻塞，直到所有日志处理完,timeout 等待处理超时时间
-func UntilFinished(timeout time.Duration, closeLogInfoChan bool) {
-	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(ctx, timeout)
-	defer cancel()
-	if closeLogInfoChan && logInfoChain != nil {
-		close(logInfoChain)
+func CloseLogChan() {
+	if closeLogChan == nil {
+		return
 	}
-	select {
-	case <-ctx.Done():
-		if !closeLogInfoChan && logInfoChain != nil {
-			close(logInfoChain)
-		}
-		<-doneChan //阻塞 确保已有的日志被处理完毕
-	case <-doneChan:
-	}
+	closeLogChan()
 }
